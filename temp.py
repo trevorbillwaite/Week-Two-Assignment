15b67858d4187a5dfbbc5c0ec768bd9667cef91b
trevorbillwaite <trev.waite@myemail.indwes.edu>
2015-09-14 18:24:16 +0000

    a

---
+++ temp.py
@@ -0,0 +1,35 @@
+# Default shell for a Python 3.x program
+# Copy this and rename it to write your own code
+#
+__author__ = 'Your Name'
+
+# Your class
+# Name of program
+#
+# Brief description of what program does.
+
+# Declare Magic Numbers and Constants
+
+# LOOPCOUNTER = 2
+# name = "bob"
+
+# Write program code here
+
+# ask user if temperature is in Farenheit
+# Convert to Celsius
+# (F-32) * 5 / 9 = C
+
+# output answer to user
+
+
+# Input
+
+F = eval(input("Please enter a temperature in Farenheit: "))
+
+C = (F-32) * 5 / 9
+
+# Process
+
+# Output
+
+print("The temperature ", F , "degrees Farenheit is equal to ", C , "degrees Celsius.")
